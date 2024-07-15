adb6041246036417f530038c351d9b67f58ae07d
Mungai Munene <mungai.munene@gmail.com>
2024-07-15 10:49:22 +0300

    updatedlab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+// returnsFirstTwoDrivers in the array
+const returnFirstTwoDrivers = function (driversArray = ['Antonia', 'Nuru', 'Amari', 'Mo']) {
+    return driversArray.slice(0,2); 
+}
+
+// returnsLastTwoDrivers in the array
+const returnLastTwoDrivers = function (driversArray = ['Antonia', 'Nuru', 'Amari', 'Mo']) {
+    return driversArray.slice(-2); 
+}
+
+// function to return both first and last two drivers
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// createFareMultiplier()
+const createFareMultiplier = function (multiplier) {
+    return function(fare) {
+        return fare * multiplier; 
+    }; 
+}; 
+
+// Fare doubler
+const fareDoubler = createFareMultiplier(2);
+
+// Fare tripler
+const fareTripler = createFareMultiplier(3);
+
+// Function to select different drivers
+const selectDifferentDrivers = function(arrayOfDrivers, selectingFunction) {
+    return selectingFunction(arrayOfDrivers);
+};
